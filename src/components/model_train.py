14e36afa19d971487546452e8a409a5f926af8c7
Azidalus <brueva.vi@gmail.com>
2025-02-23 23:55:12 +0100

    working on model_train.py

--- src/components/model_train.py
+++ src/components/model_train.py
@@ -9,3 +9,7 @@ for model in models.values():
     kf = KFold
     cv_results = 
     results.append(cv_results)
+
+for name, model in models.items():
+    model.fit(X_train, y_train)
+    test_score = model.score(X_test, y_test)
