1ad8b642b5c9ada400f889391dfc4fd376ae5bff
Azidalus <brueva.vi@gmail.com>
2025-02-23 23:52:27 +0100

    working on model_train.py

--- src/components/model_train.py
+++ src/components/model_train.py
@@ -2,3 +2,8 @@ models = {'Linear regression': LinearRegression(),
           'Ridge': Ridge(),
           'Lasso': Lasso(),
           'XGBoost': XGBoost()}
+
+results = []
+
+for model in models.values():
+    kf = KFold
