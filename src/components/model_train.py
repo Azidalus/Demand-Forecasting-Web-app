c893f3443ec9bb962361e6cc0e1dbc1f1a27d00d
Azidalus <brueva.vi@gmail.com>
2025-02-24 23:39:37 +0100

    working on model_train.py

--- src/components/model_train.py
+++ src/components/model_train.py
@@ -1,4 +1,5 @@
 from data_ingestion.py import
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
 
 models = {'Linear regression': LinearRegression(),
           'Ridge': Ridge(),
@@ -15,4 +16,4 @@ for model in models.values():
 for name, model in models.items():
     model.fit(X_train, y_train)
     test_score = model.score(X_test, y_test)
-    print("{} Test Score Accuracy: {}".format(name, test_score))
+    print("{} Test Set Accuracy: {}".format(name, test_score))
