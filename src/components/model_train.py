2e3637c3afffaff6712cc5ff9765b1072e6f1f49
Azidalus <brueva.vi@gmail.com>
2025-02-24 23:46:24 +0100

    working on model_train.py

--- src/components/model_train.py
+++ src/components/model_train.py
@@ -20,7 +20,8 @@ for name, model in models.items():
     test_score = model.score(X_test, y_test)
     print("{} Test Set Accuracy: {}".format(name, test_score))
 
-# Fine tuning
+# Final model fine tuning
 param_grid = {}
 kf = KFold
 chosen_model_cv = GridSearchCV(model, param_grid, cv=kf)
+chosen_model_cv.fit(X_train, y_train)
