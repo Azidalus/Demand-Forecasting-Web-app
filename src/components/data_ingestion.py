f8331f6e9aa95829532e3bab42edf4364cd9c948
Azidalus <brueva.vi@gmail.com>
2025-02-25 23:42:17 +0100

    working on data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,6 +2,14 @@ import os
 import sys
 from src.exception import CustomException
 from src.logger import logging
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+class DataIngestionConfig:
+    pass
+
+
 
 def get_data(csv_file, forecast_horizon):
 
