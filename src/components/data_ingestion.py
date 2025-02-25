bddc8343a01650ac9d6f9f5d3b06ca30fa5adc74
Azidalus <brueva.vi@gmail.com>
2025-02-25 23:59:57 +0100

    working on data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -26,6 +26,7 @@ class DataIngestion:
             # Create train data folder
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            logging.info('Train test split initiated')
         except:
             pass
 
