dc3e529390b80e08c2d8f2a2cfc4e76fb9b1290c
Azidalus <brueva.vi@gmail.com>
2025-02-25 23:47:45 +0100

    working on data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,7 +12,12 @@ class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', 'train.csv')
     test_data_path: str = os.path.join('artifacts', 'test.csv')
     
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
 
+    def initiate_data_ingestion(self):
+        
 
 
 def get_data(csv_file, forecast_horizon):
