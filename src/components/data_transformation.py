04db996ce428437cf886c9615acca38ffeeb7aba
Azidalus <brueva.vi@gmail.com>
2025-02-26 23:51:27 +0100

    working on data_transformation.py

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -2,4 +2,10 @@ import sys
 import numpy as np
 import pandas as pd
 from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 from dataclasses import dataclass
+
+from src.exception import CustomException
+from src.logger import logging
