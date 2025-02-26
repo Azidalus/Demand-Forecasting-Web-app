af09078ad5582e2a81388f02f6ba4e3a0d52dbd8
Azidalus <brueva.vi@gmail.com>
2025-02-26 23:41:27 +0100

    working on data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -27,10 +27,7 @@ class DataIngestion:
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
             logging.info('Train test split initiated')
+
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=1, shuffle=False)
         except:
             pass
-
-def get_data(csv_file, forecast_horizon):
-
-
-    return X_train, y_train, X_test, y_test
