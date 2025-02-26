e1dfb89104e0787ca280ac08b48177981f26e579
Azidalus <brueva.vi@gmail.com>
2025-02-26 23:44:28 +0100

    working on data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -29,5 +29,9 @@ class DataIngestion:
             logging.info('Train test split initiated')
 
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=1, shuffle=False)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.set_data_path, index=False, header=True)
+            logging.info('Train test split initiated')
+
         except:
             pass
