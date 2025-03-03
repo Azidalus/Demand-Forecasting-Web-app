d6ccec8edaa971fb07b99d86aa1590dcf7516b9c
Azidalus <brueva.vi@gmail.com>
2025-03-03 23:50:31 +0200

    working on flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -2,3 +2,9 @@ from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
 from src.pipeline.predict_pipeline import CustomData
+
+app = Flask(__name__)
+
+# Route for home page
+@app.route('/')
+def index():
