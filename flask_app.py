c0cff2d12b884ee81617020c18d44ce55e681416
Azidalus <brueva.vi@gmail.com>
2025-03-04 22:21:28 +0100

    working on flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
-from src.pipeline.predict_pipeline import CustomData
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 app = Flask(__name__)
 
