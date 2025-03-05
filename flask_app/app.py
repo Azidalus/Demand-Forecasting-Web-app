abb538fdf90181d90929885b352fadf8a537998a
Azidalus <brueva.vi@gmail.com>
2025-03-05 23:20:06 +0100

    working on flask app

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
+from fileinput import filename
 import numpy as np
 import pandas as pd
 import os
+from src.pipeline.train_pipeline import TrainPipeline
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 app = Flask(__name__)
@@ -20,26 +22,26 @@ def predict_datapoint():
         #data = CustomData(
             #date = request.form.get('date'),
             #sales = request.form.get('sales')
-        #    csv file!
         #)
 
         # Get data from forms
         forecast_horizon = request.form.get('forecast_horizon')
         data_in_csv = request.files.get('file')
+        # data_in_csv = request.files['file']
         # Extract uploaded file name
         data_filename = secure_filename(data_in_csv.filename)
         data_in_csv.save(os.path.join(app.config['UPLOAD_FOLDER'], data_filename))
         session['uploaded_data_file_path'] = os.path.join(app.config['UPLOAD_FOLDER'],
-                                             data_filename)
+                                                          data_filename)
  
-        #return render_template('index2.html')
+        #return render_template('file uploaded.html')
 
         # Convert data to pandas df
         #data_df = data.get_data_as_dataframe()
         #print(data_df)
         # Receive data and split it into train and test CSV files
         data_ingestion = DataIngestion()
-        train_path, test_path = data_ingestion.initiate_data_ingestion(data_in_csv=data)
+        train_path, test_path = data_ingestion.initiate_data_ingestion(data_in_csv=data_in_csv)
         
         # Preprocess data
         data_transformation = DataTransformation()
@@ -53,4 +55,4 @@ def predict_datapoint():
         predict_pipeline = PredictPipeline()
         results = predict_pipeline.predict(test_arr, forecast_horizon)
         
-        return render_template('home.html', accuracy=test_score*100,results=results[0])
+        return render_template('home.html', accuracy=test_score*100, results=results[0])
