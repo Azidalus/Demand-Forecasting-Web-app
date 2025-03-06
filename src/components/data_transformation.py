3db6b16d362f781c2bea815372488da21188c254
Azidalus <brueva.vi@gmail.com>
2025-03-06 23:57:12 +0100

    minor improvements

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -67,6 +67,7 @@ class DataTransformation:
             input_features_train_arr = preprocessing_obj.fit_transform(input_features_train_df)
             input_features_test_arr = preprocessing_obj.transform(input_features_test_df)
 
+            # Combine input and target features into train/test sets
             train_arr = np.c_[input_features_train_arr, np.array(target_feature_train_df)]
             test_arr = np.c_[input_features_test_arr, np.array(target_feature_test_df)]
             logging.info('Saved preprocessing object')
