02a828964ae3893b37e5fde225d6af6712c0c14a
Azidalus <brueva.vi@gmail.com>
2025-03-17 11:27:23 +0200

    building streamlit app

--- flask_app/app.py
+++ flask_app/app.py
@@ -47,11 +47,14 @@ if st.session_state['predict_btn'] == 1:
     # Train models, choose the best model and save it as .pkl 
     # Train simple arima model
     train_pipeline = TrainPipeline()
-    test_score, predictions_graph, best_model = train_pipeline.train(all_data, forecast_horizon=30)
+    test_score, test_graph, best_model = train_pipeline.train(all_data, forecast_horizon=30)
 
     # Make prediction with the best model
     predict_pipeline = PredictPipeline()
     predictions = predict_pipeline.predict(all_data, best_model, forecast_horizon=30)
+
+    # Output test predictions graph
+    #test_graph
 
 
 #app = Flask(__name__)
