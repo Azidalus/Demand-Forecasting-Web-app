49927ed930e5d0a2f4c5f19b87ef3d5632a50cc4
Azidalus <brueva.vi@gmail.com>
2025-03-21 10:35:14 +0100

    routine: adding cross-val tuning and evaluation

--- src/utils.py
+++ src/utils.py
@@ -24,15 +24,23 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
             model = list(models.values())[i]
             para = param[list(models.keys())[i]]
 
-            # Perform randomized grid search on train data and find best params
-            gs = RandomizedSearchCV(model, para, cv=3)
-            gs.fit(X_train, y_train)
-            model.set_params(**gs.best_params_)
+            if models[i] != 'ARIMA':
+                # Perform cross val grid search on data, find best params, and set them to model
+                time_series_split = TimeSeriesSplit(test_size=90)
+                gs = GridSearchCV(model, param_grid, cv=time_series_split, scoring='neg_root_mean_squared_error')
+                gs.fit(X, y)
+                model.set_params(**gs.best_params_)
 
-            # Train model
-            model.fit(X_train, y_train)
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
+                print("Best CV score: ", np.abs(gs.best_score_))
+                # Perform randomized grid search on train data and find best params
+                gs = RandomizedSearchCV(model, para, cv=3)
+                gs.fit(X_train, y_train)
+                model.set_params(**gs.best_params_)
+
+                # Train model
+                model.fit(X_train, y_train)
+                y_train_pred = model.predict(X_train)
+                y_test_pred = model.predict(X_test)
 
             train_model_score = score(y_train, y_train_pred)
             test_model_score = score(y_test, y_test_pred)
